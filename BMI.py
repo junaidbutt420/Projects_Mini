69c2016687a4fd33c83f6a761338020f616100e0
Junaid Javed <73743726+junaidbutt420@users.noreply.github.com>
2024-09-26 14:11:40 +0500

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,20 @@
+nam = input("Enter The Name ?")
+weight = int(input("Enter your Weight in pounds "))
+height = int(input("Enter your height in inchies "))
+BMI = (weight * 703) / (height * height )
+print(BMI)
+if BMI>0:
+    if BMI<18.5:
+        print(nam,', you are Underweight')
+    elif BMI<=24.9:
+        print(nam,', you are Normal Weight!')
+    elif BMI<=29.9:
+        print(nam,', you are Overweight')
+    elif BMI<=34.9:
+        print(nam,', you are Obese')
+    elif (BMI<=39.9):
+        print(nam,", you are severely obese.")
+    else:
+        print(nam,", you are morbidly obese.")
+else:
+    print("Enter valid input")
